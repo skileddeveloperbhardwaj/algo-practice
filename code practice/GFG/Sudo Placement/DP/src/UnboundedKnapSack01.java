053592c0e8876044aebafe5bf31309ec5ba1ab14
hbtidotgkb <gkb2004bhardwaj@gmail.com>
2019-08-05 08:03:26 +0900

    Project:Sudo Placement -> DP

---
+++ code practice/GFG/Sudo Placement/DP/src/UnboundedKnapSack01.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UnboundedKnapSack01 {
+	public static void main(String[] args) {
+		int tc;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Hello");
+	}
+
+}
