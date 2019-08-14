ece0785f59153779693f9a1ec89592c56445be8e
hbtidotgkb <gkb2004bhardwaj@gmail.com>
2019-08-14 07:33:46 +0530

    Claaes: Property and Fields, Getter & Setter tutorial

--- languages/Kotlin/KotlinTutorial/src/classes/PropsAndFields.kt
+++ languages/Kotlin/KotlinTutorial/src/classes/PropsAndFields.kt
@@ -1,2 +1,21 @@
 package classes
 
+open class PropsAndFields {
+    var allByDefault:Int? = null
+        get() = field //default getter
+        /*set(value) { //default setter
+            field= value
+        }*/
+        set(value) {
+            var temp = value
+            temp = temp?.plus(10)
+            field = temp
+        }
+}
+
+fun main() {
+    var propertyTest = PropsAndFields()
+    println(propertyTest.allByDefault) // getter called
+    propertyTest.allByDefault = 15 //setter called
+    println("${propertyTest.allByDefault}")
+}
