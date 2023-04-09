995af2e524ac7c3f20b40c964e2a091bc3548250
Skilled Developer Bhardwaj <skilled.developer.bhardwaj@gmail.com>
2023-04-09 16:26:40 +0530

    [LeetCode-75] Happy Number

---
+++ practice-2023/april/lc-75/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println(new HappyNumber().isHappy(19));
+        System.out.println(new HappyNumber().isHappy(7));
+    }
+}
